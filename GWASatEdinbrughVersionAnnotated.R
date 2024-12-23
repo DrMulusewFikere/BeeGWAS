6aabbe93d925149a794de9ff567553459f6f787d
DrMulusewFikere <143788575+DrMulusewFikere@users.noreply.github.com>
2024-12-23 21:14:39 +0300

    Update GWASatEdinbrughVersionAnnotated.R

--- GWASatEdinbrughVersionAnnotated.R
+++ GWASatEdinbrughVersionAnnotated.R
@@ -416,5 +416,33 @@ ggplot(pca_df, aes(x = PC1, y = PC2)) +
   labs(title = "PCA of Queen Genotype nsnpChip-2",
     x = "PCA 1",
     y = "PCA 2")
-      
-      
+
+# ----- Section 15. population structure -----
+selected_genoFile_indices <- colnames(genoQ)
+originalGeno <- pullSegSiteGeno(ahbFounderGenomeHaplo)[,selected_genoFile_indices]
+sampleInfoQ <- paste(seq(1:nrow(genoQ)),"Q", sep = "_")
+row.names(genoQ) <- sampleInfoQ
+# worker
+sampleInfoW <- paste(seq(1:nrow(poolWorkerChip2_nQTL1_h05_rwnd)),"W", sep = "_")
+row.names(poolWorkerChip2_nQTL1_h05_rwnd) <- sampleInfoW
+
+#original
+sampleInfoOrg <- paste(seq(1:nrow(originalGeno)),"O", sep = "_")
+row.names(originalGeno) <- sampleInfoOrg
+
+Q <- as.data.frame(rep("Q", nrow(genoQ)))
+colnames(Q) <- "sampleInfo"
+W <- as.data.frame(rep("W", nrow(poolWorkerChip2_nQTL1_h05_rwnd)))
+colnames(W) <- "sampleInfo"
+initialGeno <- as.data.frame(rep("initialGeno", nrow(originalGeno)))
+colnames(initialGeno) <- "sampleInfo"
+sampleInfo <- rbind(Q,W,initialGeno)
+
+# Combine all genofile
+Q_W_initialGeno <- rbind(genoQ,poolWorkerChip2_nQTL1_h05_rwnd,originalGeno)
+
+## per lineage
+subAttribute <- as.matrix(sampleInfo$sampleInfo)
+pop.gen_subAttribute <- popgen(M = Q_W_initialGeno, subgroups = subAttribute)
+pop.gen_subAttribute$bygroup$F.stats$Genotypes
+FstAttribute <- as.data.frame(pop.gen_subAttribute$bygroup$F.stats$Genotypes)
